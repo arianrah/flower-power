bac1abe8315e58470a77f22c22fee1df6a2f055d
ShawkBawks <lroberts77@gmail.com>
2019-11-22 17:22:01 -0800

    Set up initial database information/tables

--- db/schema.rb
+++ db/schema.rb
@@ -15,52 +15,52 @@ ActiveRecord::Schema.define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create table "users", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
     t.string "email"
     t.string "hashedpassword"
   end
 
-   create table "moisture_level", force: :cascade do |t|
+   create_table "moisture_levels", force: :cascade do |t|
     t.string "name"
     t.integer "watering_frequency"
    end
 
-  end
 
-  create table "sensors", force: :cascade do |t|
+  create_table "sensors", force: :cascade do |t|
     t.string "name"
   end
 
-  create table "plants", force: :cascade do |t|
+  create_table "plants", force: :cascade do |t|
     t.string "common_name"
     t.string "scientific_name"
     t.integer "max_temp"
     t.integer "min_temp"
-    add_index(:moisture_level, :id)
+    t.references :moisture_level, foreign_key: true
   end
 
-  create table "user_sensors", force: :cascade do |t|
+  create_table "user_sensors", force: :cascade do |t|
     t.integer "host"
     t.integer "gpio"
-    add_index(:sensors, :id)
-    add_index(:users, :id)
+    t.references :sensor, foreign_key: true
+    t.references :user, foreign_key: true
   end
 
-  create table "user_plants", force: :cascade do |t|
+  create_table "user_plants", force: :cascade do |t|
     t.string "name"
-    add_index(:users, :id)
-    add_index(:plants, :id)
+    t.references :user, foreign_key: true
+    t.references :plant, foreign_key: true
 end
 
-create table "groups", force: :cascade do |t|
+create_table "groups", force: :cascade do |t|
   t.string "name"
 end
 
-create table "sensors_plants_groups", force: :cascade do |t|
-  add_index(:users_sensors, :id)
-  add_index(:users_plants, :id)
-  add_index(:group_id, :id)
+create_table "sensors_plants_groups", force: :cascade do |t|
+  t.references :user_sensor, foreign_key: true
+  t.references :user_plant, foreign_key: true
+  t.references :group, foreign_key: true
 
+end
 end
