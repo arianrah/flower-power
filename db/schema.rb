bd65f6d7be3406074d902d108f1e2a6db9d42100
Lizzyfemme <lizzy_swann@hotmail.com>
2019-11-22 16:48:37 -0800

    Database schema completed

--- db/schema.rb
+++ db/schema.rb
@@ -15,4 +15,52 @@ ActiveRecord::Schema.define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "hashedpassword"
+  end
+
+   create table "moisture_level", force: :cascade do |t|
+    t.string "name"
+    t.integer "watering_frequency"
+   end
+
+  end
+
+  create table "sensors", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create table "plants", force: :cascade do |t|
+    t.string "common_name"
+    t.string "scientific_name"
+    t.integer "max_temp"
+    t.integer "min_temp"
+    add_index(:moisture_level, :id)
+  end
+
+  create table "user_sensors", force: :cascade do |t|
+    t.integer "host"
+    t.integer "gpio"
+    add_index(:sensors, :id)
+    add_index(:users, :id)
+  end
+
+  create table "user_plants", force: :cascade do |t|
+    t.string "name"
+    add_index(:users, :id)
+    add_index(:plants, :id)
+end
+
+create table "groups", force: :cascade do |t|
+  t.string "name"
+end
+
+create table "sensors_plants_groups", force: :cascade do |t|
+  add_index(:users_sensors, :id)
+  add_index(:users_plants, :id)
+  add_index(:group_id, :id)
+
 end
