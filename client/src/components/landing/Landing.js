1dee42f18c762eb920a51e8c5af2c9533d3898f6
arianrah <arianrahimi@icloud.com>
2019-11-25 11:29:40 -0800

    Registration button and form WIP. Needs post with Axios to api.

--- client/src/components/landing/Landing.js
+++ client/src/components/landing/Landing.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Dashboard from "../dashboard/Dashboard";
+import Signup from "./Signup";
+import cookieSession from "cookie-session";
 import {
   BrowserRouter as Router,
   Switch,
@@ -10,37 +12,19 @@ import {
   useLocation
 } from "react-router-dom";
 
-// This example has 3 pages: a public page, a protected
-// page, and a login screen. In order to see the protected
-// page, you must first login. Pretty standard stuff.
-//
-// First, visit the public page. Then, visit the protected
-// page. You're not yet logged in, so you are redirected
-// to the login page. After you login, you are redirected
-// back to the protected page.
-//
-// Notice the URL change each time. If you click the back
-// button at this point, would you expect to go back to the
-// login page? No! You're already logged in. Try it out,
-// and you'll see you go back to the page you visited
-// just *before* logging in, the public page.
-
 export default function Landing() {
   return (
-    <Router>
-      <div>
+    <>
+      <Router>
         <AuthButton />
 
         <Switch>
-          <Route path="/public">
-            <PublicPage />
+          <Route path="/Signup">
+            <Signup />
           </Route>
-          <PrivateRoute path="/protected">
-            <ProtectedPage />
-          </PrivateRoute>
         </Switch>
-      </div>
-    </Router>
+      </Router>
+    </>
   );
 }
 
@@ -77,38 +61,9 @@ function AuthButton() {
     <>
       <p>You are not logged in.</p>
       <LoginPage />
+      <RegisterPage />
     </>
   );
-}
-
-// A wrapper for <Route> that redirects to the login
-// screen if you're not yet authenticated.
-function PrivateRoute({ children, ...rest }) {
-  return (
-    <Route
-      {...rest}
-      render={({ location }) =>
-        fakeAuth.isAuthenticated ? (
-          children
-        ) : (
-          <Redirect
-            to={{
-              pathname: "/login",
-              state: { from: location }
-            }}
-          />
-        )
-      }
-    />
-  );
-}
-
-function PublicPage() {
-  return <h3>Public</h3>;
-}
-
-function ProtectedPage() {
-  return <h3>Protected</h3>;
 }
 
 function LoginPage() {
@@ -119,8 +74,29 @@ function LoginPage() {
   let login = () => {
     fakeAuth.authenticate(() => {
       history.replace(from);
+      // let user = request.session;
     });
   };
+  return (
+    <Link to="/">
+      <button onClick={login}>Log in</button>
+    </Link>
+  );
+}
 
-  return <button onClick={login}>Log in</button>;
+function RegisterPage() {
+  let history = useHistory();
+  let location = useLocation();
+
+  let { from } = location.state || { from: { pathname: "/" } };
+  let register = () => {
+    fakeAuth.authenticate(() => {
+      history.replace(from);
+    });
+  };
+  return (
+    <Link to="Signup">
+      <button>Register</button>
+    </Link>
+  );
 }
