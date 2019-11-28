2223c5528f471ef22e694c6c20f303918cad6115
Lizzyfemme <lizzy_swann@hotmail.com>
2019-11-28 10:58:34 -0800

    Plant card components created

--- client/src/components/plants/PlantCard.js
+++ client/src/components/plants/PlantCard.js
@@ -7,17 +7,22 @@ const plantImage = {
 const plantCard = {
   width: "70vw",
   backgroundColor: "#FFFFFF",
-  display: "Block",
+
   marginRight: "15vw",
   marginLeft: "15vw",
-  marginTop: "10vh"
+  marginTop: "10vh",
+  display: "flex",
+  justifyContent: "space-around"
+};
+const plantName = {
+  paddingBottom: "40px"
 };
 
 export default function PlantCard(props) {
   return (
     <il style={plantCard}>
       <img src={props.image} alt={props.name} style={plantImage} />
-      {props.name}
+      <p style={plantName}>{props.name}</p>
     </il>
   );
 }
